895a2bbe657743b2d7dd528ad5f170537a16abd4
wright <jeyko1@gmail.com>
2021-04-25 18:38:07 +0300

    Initialize UI

--- Assets/Scenes/PlayerItems.cs
+++ Assets/Scenes/PlayerItems.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerItems : MonoBehaviour
 {
     public float mineral_count;
+    public int player_health;
     // Start is called before the first frame update
     void Start()
     {
         mineral_count = 0.0f;
+        player_health = 3;
     }
 
 }
